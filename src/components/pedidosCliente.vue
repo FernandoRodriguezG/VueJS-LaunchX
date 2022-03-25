1a69d0f6f01d95993f441ad3c89be7e1d85fcf71
FernandoRodriguezG <fejerogo.17@gmail.com>
2022-03-24 22:40:19 -0600

    Componentes de pastelero terminados, falta componente de pedidos en el cliente

---
+++ src/components/pedidosCliente.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="pedidosCliente"><h3>
+        Haz tu pedido:
+    </h3>
+        Nombre:
+        <input type="text" id="name" v-model="$store.state.pedidos.Nombre"><br>
+        Telefono:
+    <input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" v-model="$store.state.pedidos.Telefono"><br>
+        Email:
+        <input type="email" id="email" v-model="$store.state.pedidos.email"><br>
+         Descripcion general del pastel:<input type="text" id="desc" v-model="$store.state.pedidos.descripcion"><br>
+
+         
+        Tipo de vela:
+        <input type="text" id="velas" v-model="$store.state.pedidos.adornos"><br>
+        Cantidad de velas:<input type="text" id="cantvela" v-model="$store.state.pedidos.adornos"><br>
+
+
+
+        En caso de no querer los tres sabores, solo llenar los que requieras: <br>
+        Primer sabor:<input type="text" id="firstSabor" v-model="$store.state.pedidos.sabores"><br>
+        Segundo sabor:<input type="text" id="segSabor" v-model="$store.state.pedidos.sabores"><br>
+        Sabor sabor:<input type="text" id="thirdSabor" v-model="$store.state.pedidos.sabores"><br>
+        <button @click="$store.commit('addPedido',
+        {Nombre:$store.state.adornos.Nombre,
+        Telefono:$store.state.pedidos.Telefono,
+        email:$store.state.pedidos.email,
+        descripcion:$store.state.pedidos.email,
+        adornos: $store.state.pedidos.adornos.vela,
+        sabores: $store.state.pedidos.sabores.segundo
+        })">
+        Hacer pedido</button>
+        
+        
+     </div>
+</template>
+
+<script>
+export default {
+    name:'pedidosCliente',
+
+}
+</script>
+<style scoped>
+ul,li {
+    list-style-type: none;
+}
+</style>
---
+++ src/components/pedidosPastelero.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="pedidosPastelero">
+
+        <br>
+        <h3>Pedidos para realizar:</h3>
+        <ul>
+            <li v-for="p in $store.state.pedidos" v-bind:key="p.Nombre">
+                {{p.Nombre}} -
+                {{p.Telefono}} - 
+                {{p.email}} - <br>
+                {{p.descripcion}} <br>
+                {{p.adornos.vela}} -- 
+                {{p.adornos.cantidad}} <br>
+                {{p.sabores.primero}} - 
+                {{p.sabores.segundo}} - 
+                {{p.sabores.tercero}} <br> <br>
+            </li>
+            
+        </ul>
+     </div>
+</template>
+
+<script>
+export default {
+    name:'pedidosPastelero',
+
+}
+</script>
+<style scoped>
+ul,li {
+    list-style-type: none;
+}
+</style>
